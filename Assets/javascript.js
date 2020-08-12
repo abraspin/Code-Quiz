eb0e816097ef3051e57da56e09f5d7a7fbfda946
abraspin <abrahamspindel@gmail.com>
2020-08-12 00:09:38 -0500

    creating arrays for each question/answer combo

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -11,13 +11,35 @@ var questionOneArray = [
   "3. alerts",
   "4. numbers",
 ];
-var questionTwoArray = [""];
+var questionTwoArray = ["Arrays in JavaScript can be used to store _____.",
+"1. numbers and strings",
+"2. other arrays",
+"3. booleans",
+"4. all of the above"]; 
+var questionThreeArray = ["The condition in an if / else statement is enclosed within _____.",
+"1. quotes",
+"2. curly brackets",
+"3. parentheses",
+"4. square brackets"];
+var questionFourArray = ["String values must be enclosed within _____ when being assiend to variables.",
+"1. commas",
+"2. curly brackets",
+"3. quotes",
+"4. parentheses"];
+
+var questionFiveArray = ["A very useful tool used during development and debuigging for printing content to the debugger is:",
+"1. JavaScript",
+"2. terminal / bash",
+"3. fore loops",
+"4. console.log"];
 
 $("#start-quiz-btn").on("click", function () {
   //start the quiz
   
 });
 
+//should I add an index passed as an argument for this function
+// that tells it which one is the correct answer? 
 function postNewQuestion(questionArray) {
     //first we empty the 3 rows of contents to prepare to fill them
     //with the question and answer choices
